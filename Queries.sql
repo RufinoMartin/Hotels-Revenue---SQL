749150b3b61e5126d671a80b0ce6386313db9e49
Rufino Martin <rufinomartin77@gmail.com>
2022-05-22 22:10:09 -0300

    Add files via upload

--- Queries.sql
+++ Queries.sql
@@ -61,6 +61,10 @@ order by Revenue
 
 -- Observation: 2020 is incomplete, so the revenue is reasonable.
 
+-- We also want this column to be present in our table, so 
+--   in table>desing, we make a new computed column 'Calculated Revenue' with the formula
+--     stated above: (stays_in_week_nights+stays_in_weekend_nights)*adr
+
 -- 6. We want to make use of our "Market Segment" Table.
 
 select * 
@@ -71,17 +75,38 @@ ON
 [Hotel Revenue].[dbo].[Hotels_Unions].market_segment = 
 [Hotel Revenue].[dbo].[market_segments].market_segment
 
--- 6. We also want to make use of our "Meal Costs" Table.
+-- 6. We also want to make use of our "Meal Costs" Table, and as ready, we create a view of it,
+--    so we can from now on run queries to our complete data.
+
 
 select * 
 from [Hotel Revenue].[dbo].[Hotels_Unions]
 left join 
-[Hotel Revenue].[dbo].[market_segments]
-ON
-[Hotel Revenue].[dbo].[Hotels_Unions].market_segment = 
-[Hotel Revenue].[dbo].[market_segments].market_segment
+[Hotel Revenue].[dbo].[market_segments] ON [Hotel Revenue].[dbo].[Hotels_Unions].market_segment = [Hotel Revenue].[dbo].[market_segments].market_segment
 left join 
-[Hotel Revenue].[dbo].[meal_costs]
-ON 
-[Hotel Revenue].[dbo].[meal_costs].[meal] =
-[Hotel Revenue].[dbo].[Hotels_Unions].meal
+[Hotel Revenue].[dbo].[meal_costs] ON  [Hotel Revenue].[dbo].[meal_costs].[meal] = [Hotel Revenue].[dbo].[Hotels_Unions].meal;
+
+-- Exported as XLSX and re-imported.
+
+
+
+-- Now that we have a full dataset, we proceed to run some exploratory queries
+
+-- 1. Gain knowleade over Week vs Weekends stays
+
+
+select hotel, arrival_date_year, SUM (stays_in_week_nights) as Week , SUM (stays_in_weekend_nights) as Weekend 
+FROM [Hotel Revenue].[dbo].[All]
+group by hotel, arrival_date_year
+
+
+
+
+
+SELECT
+arrival_date_year,
+hotel,
+round(sum((stays_in_week_nights+stays_in_weekend_nights)*adr),2) as Revenue
+from [Hotel Revenue].[dbo].[Hotels_Unions]
+group by arrival_date_year, hotel
+order by Revenue
